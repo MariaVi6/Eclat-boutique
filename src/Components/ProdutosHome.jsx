2a203a9345385449efe9f68e362003d679708549
mariavitoria <vitoriagaara@gmail.com>
2024-09-29 20:08:53 -0300

    terminando a home

--- src/Components/ProdutosHome.jsx
+++ src/Components/ProdutosHome.jsx
@@ -7,7 +7,7 @@ const ProdutosHome = () => {
         {/* LINHA 1 - PRODUTOS - HOME */}
         
         <section className="section-l1">
-        <div className="div-titulo-home"><h1 className="titulo-produtos">Camisas</h1></div>
+        <div className="div-titulo-home"><h1 className="titulo-produtos">Sobretudo</h1></div>
         
 
         <div className="linha1-produtos">
@@ -18,17 +18,17 @@ const ProdutosHome = () => {
         </div>
 
         <div className="tipo-de-produto1">
-        <div className="tipo1">Camisa</div>
-        <div className="tipo2">Camisa</div>
-        <div className="tipo3">Camisa</div>
-        <div className="tipo4">Camisa</div>
+        <div className="tipo1">Sobretudo</div>
+        <div className="tipo2">Sobretudo</div>
+        <div className="tipo3">Sobretudo</div>
+        <div className="tipo4">Sobretudo</div>
         </div>
 
         <div className="linha1-descricao-produtos">
-        <div className="descricao1">Camisa Verde</div>
-        <div className="descricao2">Camisa Vermelha</div>
-        <div className="descricao3">Camisa Listrada</div>
-        <div className="descricao4">Camisa Branca</div>
+        <div className="descricao1">Sobretudo Azul</div>
+        <div className="descricao2">Sobretudo Verde</div>
+        <div className="descricao3">Sobretudo Rosa</div>
+        <div className="descricao4">Sobretudo Preto</div>
         </div>
 
         <div className="valores-produtos">
