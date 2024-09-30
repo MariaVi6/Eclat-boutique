3513eb15f2b39d1e2d9fcf1b9f64e6da375d3197
mariavitoria <vitoriagaara@gmail.com>
2024-09-29 21:45:05 -0300

    foi

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -11,15 +11,15 @@ const Carousel = () => {
             </div>
             <div className="carousel-inner">
                 <div className="carousel-item active">
-                {/* "./assets/1.png" */}
+                <img src="/src/image/1-imagem-carrosel.svg" alt="" />
                     <img src="/src/assets/1.png" className="d-block w-100" alt="Slide 1" />
                 </div>
                 <div className="carousel-item">
-                {/* "./assets/1.png" */}
+                <img src="/src/image/2.svg" alt="" />
                     <img src="path/to/your/image2.jpg" className="d-block w-100" alt="Slide 2" />
                 </div>
                 <div className="carousel-item">
-                {/* "./assets/1.png" */}
+                <img src="/src/image/3.svg" alt="" />
                     <img src="path/to/your/image3.jpg" className="d-block w-100" alt="Slide 3" />
                 </div>
             </div>
