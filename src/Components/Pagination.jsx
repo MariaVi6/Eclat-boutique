d67b27ff3942e957eacd4f809eca7f3d77e30c0c
mariavitoria <vitoriagaara@gmail.com>
2024-10-19 13:45:27 -0300

    paginaion e filtro feito

---
+++ src/Components/Pagination.jsx
@@ -0,0 +1,21 @@
+const Pagination = () => {
+    return ( 
+    <>
+    <div className="pagination-produtos">
+    
+                <nav className="pagination">
+                    <ul className="pagination-ul">
+                        <li className="seta-para-esquerda"><span aria-hidden="true">&laquo;</span></li>
+                        <li className="numeros-paginacao">1</li>
+                        <li className="numeros-paginacao">2</li>
+                        <li className="numeros-paginacao">3</li>
+                        <li className="numeros-paginacao">4</li>
+                        <li className="seta-para-direita"><span aria-hidden="true">&raquo;</span></li>
+                    </ul>
+                </nav>
+        
+    </div>
+    </> );
+}
+
+export default Pagination;
