2b280b2c98c14f0fe87b5ac5ac859646e3479d9d
mariavitoria <vitoriagaara@gmail.com>
2024-10-29 15:47:01 -0300

    algumas mudanças

---
+++ src/Components/FormasPagamentos.jsx
@@ -0,0 +1,41 @@
+const FormasPagamentos = () => {
+    return ( <>
+    <div className="forma-pagamentos-home">
+
+        <div className="container-div-descricao-eclac">
+
+        <div className="div-descricao1-eclac"></div>
+        <div className="div-descricao2-eclac"></div>
+        <div className="div-descricao3-eclac"></div>
+        <div className="div-descricao4-eclac"></div>
+        <div className="div-descricao5-eclac"></div>
+        <div className="div-descricao6-eclac"></div>
+
+        </div>
+        
+        <div className="container-descricao-eclac">
+        
+        <div className="descricao-eclac">Parcelamento em 6x</div>
+        <div className="descricao-eclac">Frete Grátis</div>
+        <div className="descricao-eclac">Devolução Gratuita</div>
+        <div className="descricao-eclac">Cupom de 20%</div>
+        <div className="descricao-eclac">Entrega Rapida</div>
+        <div className="descricao-eclac">Cupom de Aniversario</div>
+
+        </div>
+
+        <div className="container-descricao2-eclac">
+        
+        <div className="descricao2-eclac"> à partir de RS400</div>
+        <div className="descricao2-eclac">à partir de R$600</div>
+        <div className="descricao2-eclac">em até 20 dias <br />após a compra</div>
+        <div className="descricao2-eclac">na primeira compra</div>
+        <div className="descricao2-eclac">para todo o brasil</div>
+        <div className="descricao2-eclac">40 off no mês <br /> de aniversário</div>
+
+        </div>
+    </div>
+    </> );
+}
+ 
+export default FormasPagamentos;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,10 +7,16 @@ const  Header  = () => {
         <div className="logo-header"></div>
 
         <div className="indice-header">
+            <div className="novidade-home">Novidades</div>
             <div className="produtos-home-header"><Link className="link-produtos" to={"/produtos"}>Produtos</Link></div>
             <div className="sobre-nos-home">Sobre Nós</div>
-            <div className="entrar-home"><Link  className="link-login" to={"/login"}>Entrar</Link></div>
         </div>
+
+        <div className="entrar-login-home">
+        <div className="entrar-home"><Link  className="link-login" to={"/login"}>Entrar</Link></div>
+        </div>
+
+        <img className="lupa-header-home" src="/src/image/lupa.png" alt="" />
         <img className="img-carrinho-header-home" src="/src/image/carrinho-de-compras.png" alt="" />
     </div>
     </> 
