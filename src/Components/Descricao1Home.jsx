21316e52c4aac58d571e81e0e22a073acf841682
mariavitoria <vitoriagaara@gmail.com>
2024-11-10 10:47:02 -0300

    mais ajustes

--- src/Components/Descricao1Home.jsx
+++ src/Components/Descricao1Home.jsx
@@ -40,7 +40,7 @@ const Descricao1Home = () => {
 
             <h1 className="titulo-cor-descricao2">Cor</h1>
 
-            <label className="titulo-cor-descricao2" for="quantidade">Quantidade</label>
+            <label className="titulo-cor-descricao2" htmlFor="quantidade">Quantidade</label>
             </div>
 
             <div className="cor-quantidade-produto">
