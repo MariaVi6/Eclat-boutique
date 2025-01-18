90e971f3d89fa2b576e2b26b435e3095df19b125
mariavitoria <vitoriagaara@gmail.com>
2025-01-18 17:08:49 -0300

    a

--- src/Components/CorpoProduto2.jsx
+++ src/Components/CorpoProduto2.jsx
@@ -35,22 +35,22 @@ const CorpoProduto2 = () => {
         <div className="valores1-produtos-pagina">
         <div className="valor1-produto-pagina">
         <div className="valor1-1-pagina-produto">R$200</div>
-        <div className="valor1-2-pagina-produto"><b>R$110</b></div>
+        <div className="valor1-2-pagina-produto">R$110</div>
         </div>
 
         <div className="valor2-produto-pagina">
         <div className="valor2-1-pagina-produto">R$400</div>
-        <div className="valor2-2-pagina-produto"><b>R$200</b></div>
+        <div className="valor2-2-pagina-produto">R$200</div>
         </div>
 
         <div className="valor3-produto-pagina">
         <div className="valor3-1-pagina-produto">R$400</div>
-        <div className="valor3-2-pagina-produto"><b>R$300</b></div>
+        <div className="valor3-2-pagina-produto">R$300</div>
         </div>
 
         <div className="valor4-produto-pagina">
         <div className="valor4-1-pagina-produto">R$200</div>
-        <div className="valor4-2-pagina-produto"><b>R$120</b></div>
+        <div className="valor4-2-pagina-produto">R$120</div>
         </div>
 
         </div>
@@ -81,22 +81,22 @@ const CorpoProduto2 = () => {
         <div className="valores1-produtos-pagina">
         <div className="valor5-produto-pagina">
         <div className="valor5-1-pagina-produto">R$200</div>
-        <div className="valor5-2-pagina-produto"><b>R$180</b></div>
+        <div className="valor5-2-pagina-produto">R$180</div>
         </div>
 
         <div className="valor6-produto-pagina">
         <div className="valor6-1-pagina-produto">R$200</div>
-        <div className="valor6-2-pagina-produto"><b>R$180</b></div>
+        <div className="valor6-2-pagina-produto">R$180</div>
         </div>
 
         <div className="valor7-produto-pagina">
         <div className="valor7-1-pagina-produto">R$200</div>
-        <div className="valor7-2-pagina-produto"><b>R$190</b></div>
+        <div className="valor7-2-pagina-produto">R$190</div>
         </div>
 
         <div className="valor8-produto-pagina">
         <div className="valor8-1-pagina-produto">R$200</div>
-        <div className="valor8-2-pagina-produto"><b>R$190</b></div>
+        <div className="valor8-2-pagina-produto">R$190</div>
         </div>
 
         </div>
@@ -118,7 +118,7 @@ const CorpoProduto2 = () => {
         </div>
 
         <div className="linha1-descricao-produtos-pagina">
-        <div className="descricao9-pagina-produto">Salto Plataforma Transparente</div>
+        <div className="descricao9-pagina-produto">Salto Transparente</div>
         <div className="descricao10-pagina-produto">Capa Bege</div>
         <div className="descricao11-pagina-produto">Brinco Ouro</div>
         <div className="descricao12-pagina-produto">Casaco Marrom</div>
@@ -127,22 +127,22 @@ const CorpoProduto2 = () => {
         <div className="valores1-produtos-pagina">
         <div className="valor9-produto-pagina">
         <div className="valor9-1-pagina-produto">R$200</div>
-        <div className="valor9-2-pagina-produto"><b>R$180</b></div>
+        <div className="valor9-2-pagina-produto">R$180</div>
         </div>
 
         <div className="valor10-produto-pagina">
         <div className="valor10-1-pagina-produto">R$200</div>
-        <div className="valor10-2-pagina-produto"><b>R$180</b></div>
+        <div className="valor10-2-pagina-produto">R$180</div>
         </div>
 
         <div className="valor11-produto-pagina">
         <div className="valor11-1-pagina-produto">R$200</div>
-        <div className="valor11-2-pagina-produto"><b>R$190</b></div>
+        <div className="valor11-2-pagina-produto">R$190</div>
         </div>
 
         <div className="valor12-produto-pagina">
         <div className="valor12-1-pagina-produto">R$200</div>
-        <div className="valor12-2-pagina-produto"><b>R$190</b></div>
+        <div className="valor12-2-pagina-produto">R$190</div>
         </div>
 
         </div>
@@ -173,22 +173,22 @@ const CorpoProduto2 = () => {
         <div className="valores1-produtos-pagina">
         <div className="valor13-produto-pagina">
         <div className="valor13-1-pagina-produto">R$200</div>
-        <div className="valor13-2-pagina-produto"><b>R$180</b></div>
+        <div className="valor13-2-pagina-produto">R$180</div>
         </div>
 
         <div className="valor14-produto-pagina">
         <div className="valor14-1-pagina-produto">R$200</div>
-        <div className="valor14-2-pagina-produto"><b>R$180</b></div>
+        <div className="valor14-2-pagina-produto">R$180</div>
         </div>
 
         <div className="valor15-produto-pagina">
         <div className="valor15-1-pagina-produto">R$200</div>
-        <div className="valor15-2-pagina-produto"><b>R$190</b></div>
+        <div className="valor15-2-pagina-produto">R$190</div>
         </div>
 
         <div className="valor16-produto-pagina">
         <div className="valor16-1-pagina-produto">R$200</div>
-        <div className="valor16-2-pagina-produto"><b>R$190</b></div>
+        <div className="valor16-2-pagina-produto">R$190</div>
         </div>
 
         </div>
@@ -219,22 +219,22 @@ const CorpoProduto2 = () => {
         <div className="valores1-produtos-pagina">
         <div className="valor17-produto-pagina">
         <div className="valor17-1-pagina-produto">R$200</div>
-        <div className="valor17-2-pagina-produto"><b>R$180</b></div>
+        <div className="valor17-2-pagina-produto">R$180</div>
         </div>
 
         <div className="valor18-produto-pagina">
         <div className="valor18-1-pagina-produto">R$200</div>
-        <div className="valor18-2-pagina-produto"><b>R$180</b></div>
+        <div className="valor18-2-pagina-produto">R$180</div>
         </div>
 
         <div className="valor19-produto-pagina">
         <div className="valor19-1-pagina-produto">R$200</div>
-        <div className="valor19-2-pagina-produto"><b>R$190</b></div>
+        <div className="valor19-2-pagina-produto">R$190</div>
         </div>
 
         <div className="valor20-produto-pagina">
         <div className="valor20-1-pagina-produto">R$200</div>
-        <div className="valor20-2-pagina-produto"><b>R$190</b></div>
+        <div className="valor20-2-pagina-produto">R$190</div>
         </div>
 
         </div>
@@ -265,22 +265,22 @@ const CorpoProduto2 = () => {
         <div className="valores1-produtos-pagina">
         <div className="valor21-produto-pagina">
         <div className="valor21-1-pagina-produto">R$200</div>
-        <div className="valor21-2-pagina-produto"><b>R$180</b></div>
+        <div className="valor21-2-pagina-produto">R$180</div>
         </div>
 
         <div className="valor22-produto-pagina">
         <div className="valor22-1-pagina-produto">R$200</div>
-        <div className="valor22-2-pagina-produto"><b>R$180</b></div>
+        <div className="valor22-2-pagina-produto">R$180</div>
         </div>
 
         <div className="valor23-produto-pagina">
         <div className="valor23-1-pagina-produto">R$200</div>
-        <div className="valor23-2-pagina-produto"><b>R$190</b></div>
+        <div className="valor23-2-pagina-produto">R$190</div>
         </div>
 
         <div className="valor24-produto-pagina">
         <div className="valor24-1-pagina-produto">R$200</div>
-        <div className="valor24-2-pagina-produto"><b>R$190</b></div>
+        <div className="valor24-2-pagina-produto">R$190</div>
         </div>
 
 </div>
@@ -311,22 +311,22 @@ const CorpoProduto2 = () => {
         <div className="valores1-produtos-pagina">
         <div className="valor25-produto-pagina">
         <div className="valor25-1-pagina-produto">R$200</div>
-        <div className="valor25-2-pagina-produto"><b>R$180</b></div>
+        <div className="valor25-2-pagina-produto">R$180</div>
         </div>
 
         <div className="valor26-produto-pagina">
         <div className="valor26-1-pagina-produto">R$200</div>
-        <div className="valor26-2-pagina-produto"><b>R$180</b></div>
+        <div className="valor26-2-pagina-produto">R$180</div>
         </div>
 
         <div className="valor27-produto-pagina">
         <div className="valor27-1-pagina-produto">R$200</div>
-        <div className="valor27-2-pagina-produto"><b>R$190</b></div>
+        <div className="valor27-2-pagina-produto">R$190</div>
         </div>
 
         <div className="valor28-produto-pagina">
         <div className="valor28-1-pagina-produto">R$200</div>
-        <div className="valor28-2-pagina-produto"><b>R$190</b></div>
+        <div className="valor28-2-pagina-produto">R$190</div>
         </div>
 
         </div>
@@ -357,22 +357,22 @@ const CorpoProduto2 = () => {
         <div className="valores1-produtos-pagina">
         <div className="valor29-produto-pagina">
         <div className="valor29-1-pagina-produto">R$200</div>
-        <div className="valor29-2-pagina-produto"><b>R$180</b></div>
+        <div className="valor29-2-pagina-produto">R$180</div>
         </div>
 
         <div className="valor30-produto-pagina">
         <div className="valor30-1-pagina-produto">R$200</div>
-        <div className="valor30-2-pagina-produto"><b>R$180</b></div>
+        <div className="valor30-2-pagina-produto">R$180</div>
         </div>
 
         <div className="valor31-produto-pagina">
         <div className="valor31-1-pagina-produto">R$200</div>
-        <div className="valor31-2-pagina-produto"><b>R$190</b></div>
+        <div className="valor31-2-pagina-produto">R$190</div>
         </div>
 
         <div className="valor32-produto-pagina">
         <div className="valor32-1-pagina-produto">R$200</div>
-        <div className="valor32-2-pagina-produto"><b>R$190</b></div>
+        <div className="valor32-2-pagina-produto">R$190</div>
         </div>
 
 </div>
